9344ccc3a21ffde7afab3ceae1fef873f35628ba
jkhenrich <jkhenrich@github.com>
2015-06-30 17:12:50 -0400

    Standard deviation and correlation functions added

--- RunConvolutedData.py
+++ RunConvolutedData.py
@@ -15,7 +15,7 @@ import FourierTransform as ft
 
 
 def run_the_transform (e_end, ne, sig):
-    number_of_runs = 40
+    number_of_runs = 100
     e = np.linspace(-e_end, e_end, ne)
     t  = np.linspace((-pi*ne)/(4*e_end), (pi*ne)/(4*e_end), ne)
     pretransformedvalues = np.ndarray(shape = (number_of_runs, ne), dtype=float)
@@ -32,42 +32,63 @@ def run_the_transform (e_end, ne, sig):
         pylab.plot(t, iftconvoi, '.')
 #        pylab.plot(t, iftconvoi / iftconvoi[(ne-1)/2], '.') #plottingline
         point_means +=  iftconvoi 
-        point_stds += iftconvoi**2
-    #point_stds = sqrt((1 / (number_of_runs - 1))*(point_stds +(1/number_of_runs)*(point_means**2)))
-    point_stds = (1 / (number_of_runs - 1))*(point_stds +(1/number_of_runs)*(point_means**2))
+        point_stds += np.multiply(iftconvoi, iftconvoi)
+    #point_stds = sqrt((1 / (number_of_runs))*(point_stds + (1/number_of_runs)*(point_means**2)))
+    point_stds = sqrt((1 / (number_of_runs - 1))*(point_stds +(1/number_of_runs)*(point_means**2)))
+    print point_stds
     point_means = (1 / number_of_runs)*point_means
-    return number_of_runs, alliftvalues, point_means#t, point_means, point_stds#, allvalues
+    return number_of_runs, alliftvalues, point_means, t, point_stds#, allvalues
 
-def find_correlation2rows(number_of_runs, data, point_means):
-    firstdesrow = data[2]
-    seconddesrow = data[3]
-    firstmean = point_means[2]
-    secondmean = point_means[3]
+def find_covariance(firstdesrowmmean, secdesrowmmean):
+    tosum = firstdesrowmmean * secdesrowmmean
+    covariance = np.sum(tosum) / (number_of_runs-1)
+    return covariance
+
+def find_stds(rowmmean, number_of_runs):
+    firstrowsqr = np.multiply(rowmmean, rowmmean)
+    stdfirstrow = sqrt(np.sum(firstrowsqr)/(number_of_runs-1))
+    return stdfirstrow
+
+
+def find_correlation2rows(i, number_of_runs, data, point_means):
+    firstdesrow = data[i]
+    seconddesrow = data[i+1]
+    firstmean = point_means[i]
+    secondmean = point_means[i+1]
     #To calculate the covariance
     firstdesrowmmean = firstdesrow - firstmean
     secdesrowmmean = seconddesrow - secondmean
-    tosum = firstdesrowmmean * secdesrowmmean
-    covariance = np.sum(tosum) / (number_of_runs-1)
+    covariance = find_covariance(firstdesrowmmean, secdesrowmmean)
     #To calculate the standarddeviations
-    firstrowsqr = firstdesrow * firstdesrow
-    secondrowsqr = seconddesrow * seconddesrow
-    stdfirstrow = sqrt(np.sum(firstrowsqr)/(number_of_runs-1))
-    stdsecondrow = sqrt(np.sum(secondrowsqr)/(number_of_runs-1))
+    stdfirstrow = find_stds(firstdesrowmmean, number_of_runs)
+    stdsecondrow = find_stds(secdesrowmmean, number_of_runs)
     correlation = covariance / (stdfirstrow*stdsecondrow)
     return correlation
       
-def plot_transform_run(returnedvectors):
-    t, point_means, point_stds = returnedvectors
-    number_of_runs = 100
+def plot_transform_run(number_of_runs, t, point_means, point_stds):
 #    pylab.plot(t, point_stds, '.')
 #    for i in np.arange(number_of_runs):
 #        pylab.plot(t, allvalues[i], '.')
     pylab.errorbar(t, point_means, yerr=point_stds)
     
-#pylab.clf()
-#run_the_transform(1.5, 2251, 0.01)
-#plot_transform_run(run_the_transform(1.5, 2251, 0.01))
-#pylab.show()
-#dc.export_data_csv(run_the_transform(1.5, 2251, 0.01), 'originaldistvalues1')
-number_of_runs, data, point_means = run_the_transform(1.5, 2251, 0.01)    
-print find_correlation2rows(number_of_runs, data, point_means)
+    
+def demo_correlation_btw_neighbors(number_of_runs, data, point_means):
+    correlations = np.empty_like(np.arange(len(point_means)-1))
+    #for i in np.arange(len(point_means)-1):  
+    correlations= [find_correlation2rows(i, number_of_runs, data.transpose(), point_means) for i in np.arange(len(point_means)-1)]
+    print correlations
+
+def demo_standard_deviations(number_of_runs, data, point_means):
+    standarddeviations = np.empty_like(point_means)
+    standarddeviations = [find_stds((data.transpose()[i] - point_means[i]), number_of_runs) for i in np.arange(len(point_means))]
+    return np.array(standarddeviations)
+
+number_of_runs, data, point_means, t, point_stds = run_the_transform(1.5, 2251, 0.01)
+#demo_correlation_btw_neighbors(number_of_runs, data, point_means)
+stdscalc = demo_standard_deviations(number_of_runs, data, point_means)
+print stdscalc
+pylab.clf()
+plot_transform_run(number_of_runs, t, point_means, point_stds)
+pylab.show()
+dc.export_data_csv(data, 'originaldistvalues3')
+dc.export_data_csv(np.vstack([point_stds, stdscalc]), 'stdscalc2ways_data3')
